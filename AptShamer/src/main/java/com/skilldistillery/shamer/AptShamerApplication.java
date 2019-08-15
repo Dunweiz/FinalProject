6078661cef65aabc781f0db8b6ba3cb432838596
Dunweiz8157 <dunweiz8157@gmail.com>
2019-08-15 14:53:54 -0600

    Stub Boot project, link JPA dependency

---
+++ AptShamer/src/main/java/com/skilldistillery/shamer/AptShamerApplication.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.shamer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AptShamerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AptShamerApplication.class, args);
+	}
+
+}
