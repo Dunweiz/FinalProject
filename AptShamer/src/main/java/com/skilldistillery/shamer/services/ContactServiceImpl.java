1df9f41699c65ad9291466f7737349472b493d9b
Chana Cohn <chana.cohn@gmail.com>
2019-08-16 15:05:46 -0600

    working on contacts

---
+++ AptShamer/src/main/java/com/skilldistillery/shamer/services/ContactServiceImpl.java
@@ -0,0 +1,34 @@
+package com.skilldistillery.shamer.services;
+
+import java.util.List;
+
+import com.skilldistillery.shamer.entities.Contact;
+
+public class ContactServiceImpl implements ContactService {
+
+	@Override
+	public List<Contact> index() {
+		return null;
+	}
+
+	@Override
+	public Contact show(int id) {
+		return null;
+	}
+
+	@Override
+	public Contact create(Contact contact) {
+		return null;
+	}
+
+	@Override
+	public Contact update(int id, Contact contact) {
+		return null;
+	}
+
+	@Override
+	public Boolean destroy(int id) {
+		return null;
+	}
+
+}
