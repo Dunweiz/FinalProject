80c63541c9d7b1637024f598d078613433f690a7
Dunweiz8157 <dunweiz8157@gmail.com>
2019-08-17 13:45:34 -0600

    completed some models

--- ngAptShamer/src/app/models/complex.ts
+++ ngAptShamer/src/app/models/complex.ts
@@ -1,2 +1,35 @@
+import { Contact } from './contact';
+import { Complaint } from './complaint';
+
 export class Complex {
+  id: number;
+  name: string;
+  street: string;
+  city: string;
+  state: string;
+  zip: string;
+  imageUrl: string;
+  numUnits: number;
+  contacts: Contact[];
+  complaints: Complaint[];
+  profiles: UserProfile[];
+  ratings: Rating[];
+
+  constructor(id?: number, name?: string, street?: string, city?: string,
+              state?: string, zip?: string, imageUrl?: string, numUnits?: number,
+              contacts?: Contact[], complaints?: Complaint[], profiles?: userProfile,
+              ratings?: Rating[]) {
+                this.id = id;
+                this.name = name;
+                this.street = street;
+                this.city = city;
+                this.state = state;
+                this.zip = zip;
+                this.imageUrl = imageUrl;
+                this.numUnits = numUnits;
+                this.contacts = contacts;
+                this.complaints = complaints;
+                this.profiles = profiles;
+                this.ratings = ratings;
+              }
 }
