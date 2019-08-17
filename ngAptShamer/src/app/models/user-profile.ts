35222d7d205ed45896b6d13480bd85dc55dce9e1
Ryan Harrington <harrington.ryana@gmail.com>
2019-08-17 13:15:01 -0600

    Create userProfile model

--- ngAptShamer/src/app/models/user-profile.ts
+++ ngAptShamer/src/app/models/user-profile.ts
@@ -1,12 +1,36 @@
+import { Rating } from './rating';
+import { Complaint } from './complaint';
+import { Complex } from './complex';
 
 
 export class UserProfile {
-  id: number;
-  user: User;
+  comments: Comment[];
+  complaints: Complaint[];
   complex: Complex;
+  displayName: string;
+  email: string;
+  firstName: string;
+  id: number;
+  imageUrl: string;
+  lastName: string;
+  ratings: Rating[];
+  user: User;
 
-  constructor(id?: number, user?: User, ) {
+
+  constructor(comments?: Comment[], complaints?: Complaint[], complex?: Complex,
+              displayName?: string, email?: string, firstName?: string, id?: number,
+              imageUrl?: string, lastName?: string, ratings?: Rating[], user?: User, ) {
     this.id = id;
+    this.user = user;
+    this.complaints = complaints;
+    this.complex = complex;
+    this.displayName = displayName;
+    this.email = email;
+    this.firstName = firstName;
+    this.id = id;
+    this.imageUrl = imageUrl;
+    this.lastName = lastName;
+    this.ratings = ratings;
     this.user = user;
   }
 
