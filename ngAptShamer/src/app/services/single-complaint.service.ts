7df039d2d1cba4cd5e892491440c66ec9b84da5b
hatle$ <kayla.hatle@yahoo.com>
2019-08-19 13:37:51 -0600

    update readme

---
+++ ngAptShamer/src/app/services/single-complaint.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SingleComplaintService {
+  // Fields
+
+  // private url = environment.baseUrl + 'api/'
+
+  // Contructor
+
+  constructor() { }
+
+  // Methods
+
+
+
+}
