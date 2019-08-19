74f89d39af75c3c039ca329f948d98fd40d081ca
Chana Cohn <chana.cohn@gmail.com>
2019-08-19 13:55:45 -0600

    beginning template

--- ngAptShamer/src/app/app.module.ts
+++ ngAptShamer/src/app/app.module.ts
@@ -1,22 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { RatingPipe } from './pipes/rating.pipe';
 import { NavigationComponent } from './components/navigation/navigation.component';
+import { TemplateComponent } from './components/template/template.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     RatingPipe,
-    NavigationComponent
+    NavigationComponent,
+    TemplateComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
